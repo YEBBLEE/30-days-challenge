19f8b19e4a917499f6491a8c41c12a9a35542fcb
YEBBLEE <leeyeb9458@gmail.com>
2022-05-10 16:51:36 +0900

    Routes컴포넌트에 있던  Challenges컴포넌트와 관련된 함수 Challenges컴포넌트에서 관리하도록 변경함 & Challenges컴포넌트가 최초로 렌더링 될때 해당유저의 챌린지만을 가져오도록 componentDidMout()에 코드 추가

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -7,13 +7,11 @@ import {
   Route,
   Redirect
 } from 'react-router-dom';
-import ProgressCount from './ProgressCount';
 import Navbar from './Navbar';
 
 class Routes extends Component {
   
   state = {
-    challenges : [],
     user: undefined
   }
 
@@ -51,117 +49,8 @@ class Routes extends Component {
       .catch(error => console.log('error', error));
   };
 
-  componentDidMount() {
-    // if (this.isToken && this.nickname) {
-    //   const query = nickname ? `?nickname=${this.nickname}` : '';
-    //   const reqOptions = {
-    //     method: 'GET',
-    //     redirect: 'follow',
-    //     headers: {
-    //       'Content-Type': 'application/json'
-    //     }
-    //   };
-          
-    //   fetch(`http://localhost:8080/challenges${query}`, reqOptions)
-    //     .then(res => {
-    //       const result = res.json();
-    //       if(res.status > 299 || res.status < 200) {
-    //         const msg = result && result.message ? result.message : 'Something Wrong!';
-    //         const error = new Error(msg);
-  
-    //         throw error;
-    //       }
-    //       return result;
-    //     })
-    //     .then(result => {
-    //       console.log(result);
-    //       this.setState({challenges: result});
-    //     })
-    //     .catch(error => console.log('error', error));  
-    // }else {
-    //   return
-    // }
-  }
-  
-  componentWillUnmount() {
-
-  }
-
-  handleStart = (title) => {
-    const result = this.http.sendRequest
-    ('/challenges',{
-      method: 'POST',
-      body: JSON.stringify({title,nickname:'YEBIN'})
-    });
-    
-    result
-      .then((result) => {
-        const challenge = result;
-        const challenges = [challenge, ...this.state.challenges];
-        this.setState({challenges});
-      })
-      .catch((err) => console.log('error',err));
-  }
-  
-  handleModify = (title,challenge) => {
-    const challengeId = challenge.id;
-
-    const result = this.http.sendRequest
-    (`/challenges/${challengeId}`,
-      { method: 'PUT',body: JSON.stringify({title,nickname:'YEBIN'})}
-    );
-
-    result 
-      .then((result) => {
-        const modified = result;
-        let challenges = [...this.state.challenges];
-        challenges.splice(challenges.indexOf(challenge),1,modified);
-        this.setState({challenges});
-      })
-      .catch((err) => console.log('error',err));
-  }
-
-  handleDelete = (challenge) => {
-    const challengeId = challenge.id;
-
-    const result = this.http.sendRequest
-    (`/challenges/${challengeId}`,{ method: 'DELETE'});
-
-    result
-    .then(() => {
-      const challenges = this.state.challenges.filter(challenge => challenge.id !== challengeId );
-      this.setState({challenges});
-    })
-    .catch(error => console.log('error', error));
-  }
-
-  handleNumber = (challenge,days,isChecked,number) => {
-    const daysId = days.id;
-
-    const result = this.http.sendRequest
-    (`/challenges/days/${daysId}`,{
-      method: 'PUT',
-      body : JSON.stringify({number,isChecked})
-    });
-
-    result
-      .then((result) => {
-        let challenges = [...this.state.challenges];
-        const chIdx = challenges.indexOf(challenge);
-        const modifiedChallenge = result;
-        challenges.splice(chIdx,1,modifiedChallenge);
-        this.setState({challenges});
-      })
-      .catch(error => console.log('error', error));
-  }
-
-  setEndDate = () => {
-    let date = new Date();
-    date.setDate(date.getDate() + 30);
-    return date.toLocaleDateString();
-  }
-
   render() {
+    console.log('Routes컴포넌트 Render');
     return (
       <>
         <Navbar 
@@ -176,16 +65,9 @@ class Routes extends Component {
               onLogin = {this.handleLogin}/>
           </Route>
           <Route path="/challenges">
-              <ProgressCount 
-                countNumber={this.state.challenges.filter(challenge => challenge.isProgress).length}
-              />
               <Challenges
-              challenges={this.state.challenges}
-              onStart={this.handleStart}
-              onDelete={this.handleDelete}
-              onModify={this.handleModify}
-              onNumberClicked={this.handleNumber}
-            />
+                http = {this.http}
+                user = {this.state.user}/>
           </Route>
         </Switch>
       </>
--- src/components/challenges.jsx
+++ src/components/challenges.jsx
@@ -1,43 +1,148 @@
 import React, { Component } from 'react';
 import Challenge from './Challenge';
 import InputForm from './InputForm';
+import ProgressCount from './ProgressCount';
 
 class Challenges extends Component {
+
+    state = {
+        challenges : []
+    }
+
+    http = this.props.http;
+
+    componentDidMount() {
+        console.log(`[Challenges] DidMount!! : ${this.props.user}`);
+        if (!this.props.user) {
+            return
+        }
+        const nickname = this.props.user.nickname;
+        console.log(nickname);
+        const query = nickname ? `?nickname=${nickname}` : '';
+        const reqOptions = {
+            method: 'GET',
+            redirect: 'follow',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+        
+        fetch(`http://localhost:8080/challenges${query}`, reqOptions)
+            .then(res => {
+                const result = res.json();
+                if(res.status > 299 || res.status < 200) {
+                    const msg = result && result.message ? result.message : 'Something Wrong!';
+                    const error = new Error(msg)
+                    throw error;
+                }
+                return result;
+            })
+            .then(result => {
+                console.log(result);
+                this.setState({challenges: result});
+            })
+            .catch(error => console.log('error', error));  
+    }
+
     handleStart = (title) => {
-        this.props.onStart(title);
+        const result = this.http.sendRequest
+        ('/challenges',{
+            method: 'POST',
+            body: JSON.stringify({title,nickname:`${this.props.user.nickname}`})
+        });
+        
+        result
+            .then((result) => {
+                const challenge = result;
+                const challenges = [challenge, ...this.state.challenges];
+                this.setState({challenges});
+            })
+            .catch((err) => console.log('error',err));
     }
 
     handleDelete = (challenge) => {
-        this.props.onDelete(challenge);
+        const challengeId = challenge.id;
+
+        const result = this.http.sendRequest
+        (`/challenges/${challengeId}`,{ method: 'DELETE'});
+    
+        result
+        .then(() => {
+            const challenges = this.state.challenges.filter(challenge => challenge.id !== challengeId );
+            this.setState({challenges});
+        })
+        .catch(error => console.log('error', error));
     }
     
     handleModify = (title,challenge) => {
-        this.props.onModify(title,challenge);
-    }
+        const challengeId = challenge.id;
+
+        const result = this.http.sendRequest
+        (`/challenges/${challengeId}`,
+            { method: 'PUT',body: JSON.stringify({title,nickname:`${this.props.user.nickname}`})}
+        );
     
-    handleNumber = (challenge,days,isClicked,number) => {
-        this.props.onNumberClicked(challenge,days,isClicked,number);
+        result 
+            .then((result) => {
+                const modified = result;
+                let challenges = [...this.state.challenges];
+                challenges.splice(challenges.indexOf(challenge),1,modified);
+                this.setState({challenges});
+            })
+            .catch((err) => console.log('error',err));
+    }
+
+    handleNumber = (challenge,days,isChecked,number) => {
+        const daysId = days.id;
+
+        const result = this.http.sendRequest
+        (`/challenges/days/${daysId}`,{
+            method: 'PUT',
+            body : JSON.stringify({number,isChecked})
+        });
+    
+        result
+            .then((result) => {
+                let challenges = [...this.state.challenges];
+                const chIdx = challenges.indexOf(challenge);
+                const modifiedChallenge = result;
+                challenges.splice(chIdx,1,modifiedChallenge);
+                this.setState({challenges});
+            })
+            .catch(error => console.log('error', error));
+    }
+
+    setEndDate = () => {
+        let date = new Date();
+        date.setDate(date.getDate() + 30);
+        return date.toLocaleDateString();
     }
 
     render() {
         return (
+        <>
+        {this.props.user && (
             <>
-                <InputForm
-                    onStart = {this.handleStart}
-                />
-                <ul className='challenges'>
-                    {this.props.challenges.map(challenge => 
-                        <Challenge
-                            key={challenge.id}
-                            challenge={challenge}
-                            onDelete={this.handleDelete}
-                            onModify={this.handleModify}
-                            onNumberClicked={this.handleNumber}
-                        />
-                    )}
-                </ul>
+            <ProgressCount 
+            countNumber={this.state.challenges.filter(challenge => challenge.isProgress).length}
+            />
+            <InputForm
+                onStart = {this.handleStart}
+            />
+            <ul className='challenges'>
+                {this.state.challenges.map(challenge => 
+                    <Challenge
+                        key={challenge.id}
+                        challenge={challenge}
+                        onDelete={this.handleDelete}
+                        onModify={this.handleModify}
+                        onNumberClicked={this.handleNumber}
+                    />
+                )}
+            </ul>
             </>
-            
+        )}
+        </>
         );
     }
 }
